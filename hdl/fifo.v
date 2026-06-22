189bfa573e6e82510aedd0ed096de24574364244
luanzd <diegoluandenil@hotmail.com>
2026-06-22 11:31:18 -0300

    Fifo finalizado e pronto para ser testado

--- hdl/fifo.v
+++ hdl/fifo.v
@@ -7,7 +7,7 @@
 
 module fifo #(
     parameter DEPTH = 4,               // Profundidade padrão para UP/DN
-    parameter WIDTH = `SPIN_CHANNEL_W  // 36 bits
+    parameter WIDTH = `SPIN_CHANNEL_W  // 36 bits definido no config.vh
 ) (
     input wire clk,
     input wire rst,
@@ -30,8 +30,7 @@ module fifo #(
     // Função interna do Verilog-2001 para calcular a largura dos ponteiros
     localparam PTR_WIDTH = $clog2(DEPTH);
     //Define o valor máximo do ponteiro com a largura exata
-    localparam [PTR_WIDTH-1:0] MAX_PTR = DEPTH - 1;
-
+    localparam [PTR_WIDTH-1:0] MAX_PTR = PTR_WIDTH'(DEPTH - 1);
     // Array de memória
     reg [WIDTH-1:0] mem[0:DEPTH-1];
 
@@ -49,9 +48,9 @@ module fifo #(
     // Bloco sequencial para controle da FIFO
     always @(posedge clk) begin
         if (rst) begin
-            wptr <= (wptr == DEPTH - 1) ? 0 : wptr + 1;
-            rptr <= (rptr == DEPTH - 1) ? 0 : rptr + 1;
-            count    <= 0;
+            wptr <= 0;
+            rptr <= 0;
+            count <= 0;
             data_out <= 0;
         end else begin
             // ----------------------------------------------------
@@ -64,22 +63,22 @@ module fifo #(
                 2'b10: begin  // Apenas Escrita
                     mem[wptr] <= data_in;
                     // Lógica circular do ponteiro
-                    wptr <= (wptr == DEPTH - 1) ? 0 : wptr + 1;
+                    wptr <= (wptr == MAX_PTR) ? 0 : wptr + 1;
                     count <= count + 1;
                 end
 
                 2'b01: begin  // Apenas Leitura
                     data_out <= mem[rptr];
-                    rptr <= (rptr == DEPTH - 1) ? 0 : rptr + 1;
+                    rptr <= (rptr == MAX_PTR) ? 0 : rptr + 1;
                     count <= count - 1;
                 end
 
-                2'b11: begin  // Escrita e Leitura Simultâneas
+                2'b11: begin  // Escrita e Leitura 
                     mem[wptr] <= data_in;
                     data_out <= mem[rptr];
 
-                    wptr <= (wptr == DEPTH - 1) ? 0 : wptr + 1;
-                    rptr <= (rptr == DEPTH - 1) ? 0 : rptr + 1;
+                    wptr <= (wptr == MAX_PTR) ? 0 : wptr + 1;
+                    rptr <= (rptr == MAX_PTR) ? 0 : rptr + 1;
                     // count se mantém igual, pois entrou 1 e saiu 1
                 end
 
